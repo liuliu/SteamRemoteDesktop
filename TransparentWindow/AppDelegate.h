f227ccf0eb39c829639e053b6fb978066bb7c78f
Liu Liu <i@liuliu.me>
2020-06-13 17:44:39 -0700

    Initial Commit

---
+++ TransparentWindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TransparentWindow
+//
+//  Created by Liu Liu on 6/13/20.
+//  Copyright © 2020 Liu Liu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TransparentWindow/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  TransparentWindow
+//
+//  Created by Liu Liu on 6/13/20.
+//  Copyright © 2020 Liu Liu. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+  // Insert code here to tear down your application
+}
+
+
+@end
